ccf532c9cd759b8a1e48cb77a990904b6ceaff84
Jan Hoffmann <jan@hfmn.pl>
2019-02-15 13:55:25 +0100

    doxygen stuff

--- Adafruit_CAP1188.cpp
+++ Adafruit_CAP1188.cpp
@@ -1,18 +1,32 @@
-/*************************************************** 
-  This is a library for the CAP1188 I2C/SPI 8-chan Capacitive Sensor
-
-  Designed specifically to work with the CAP1188 sensor from Adafruit
-  ----> https://www.adafruit.com/products/1602
-
-  These sensors use I2C/SPI to communicate, 2+ pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+/*!
+ *  @file Adafruit_CAP1188.cpp
+ *
+ *  @mainpage Adafruit CAP1188 I2C/SPI 8-chan Capacitive Sensor
+ *
+ *  @section intro_sec Introduction
+ *
+ * 	This is a library for the Adafruit CAP1188 I2C/SPI 8-chan Capacitive
+ * Sensor http://www.adafruit.com/products/1602
+ *
+ *  These sensors use I2C/SPI to communicate, 2+ pins are required to
+ *  interface
+ *
+ * 	Adafruit invests time and resources providing this open source code,
+ *  please support Adafruit and open-source hardware by purchasing products from
+ * 	Adafruit!
+ *
+ *  @section author Author
+ *
+ *  Limor Fried/Ladyada (Adafruit Industries).
+ *
+ * 	@section license License
+ *
+ * 	BSD (see license.txt)
+ *
+ * 	@section  HISTORY
+ *
+ *     v1.0 - First release
+ */
 
 #include "Adafruit_CAP1188.h"
 
@@ -20,29 +34,40 @@
 // establish settings and protect from interference from other
 // libraries.  Otherwise, they simply do nothing.
 #ifdef SPI_HAS_TRANSACTION
-  static inline void spi_begin(void) __attribute__((always_inline));
-  static inline void spi_begin(void) {
-    // max speed!
-    SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-  }
-  static inline void spi_end(void) __attribute__((always_inline));
-  static inline void spi_end(void) {
-    SPI.endTransaction();
-  }
-#else
-  #define spi_begin()
-  #define spi_end()
+static inline void spi_begin(void) __attribute__((always_inline));
+static inline void spi_begin(void) {
+  // max speed!
+  SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
+}
+static inline void spi_end(void) __attribute__((always_inline));
+static inline void spi_end(void) { SPI.endTransaction(); }
+/* #else */
+/*   #define spi_begin() */
+/*   #define spi_end() */
 #endif
 
+/* uint8_t mySPCR, SPCRback; */
 
-uint8_t mySPCR, SPCRback;
-
+/*!
+ *    @brief  Instantiates a new CAP1188 class using hardware I2C
+ *    @param  resetpin
+ *            number of pin where reset is connected
+ *
+ */
 Adafruit_CAP1188::Adafruit_CAP1188(int8_t resetpin) {
   // I2C
   _resetpin = resetpin;
   _i2c = true;
 }
 
+/*!
+ *    @brief  Instantiates a new CAP1188 class using hardware SPI
+ *    @param  cspin
+ *            number of CSPIN (Chip Select)
+ *    @param  resetpin
+ *            number of pin where reset is connected
+ *
+ */
 Adafruit_CAP1188::Adafruit_CAP1188(int8_t cspin, int8_t resetpin) {
   // Hardware SPI
   _cs = cspin;
@@ -51,9 +76,23 @@ Adafruit_CAP1188::Adafruit_CAP1188(int8_t cspin, int8_t resetpin) {
   _i2c = false;
 }
 
-Adafruit_CAP1188::Adafruit_CAP1188(int8_t clkpin, int8_t misopin, 
-				   int8_t mosipin,int8_t cspin, 
-				   int8_t resetpin) {
+/*!
+ *    @brief  Instantiates a new CAP1188 class using software SPI
+ *    @param  clkpin
+ *            number of pin used for CLK (clock pin)
+ *    @param  misopin
+ *            number of pin used for MISO (Master In Slave Out)
+ *    @param  mosipin
+ *            number of pin used for MOSI (Master Out Slave In))
+ *    @param  cspin
+ *            number of CSPIN (Chip Select)
+ *    @param  resetpin
+ *            number of pin where reset is connected
+ *
+ */
+Adafruit_CAP1188::Adafruit_CAP1188(int8_t clkpin, int8_t misopin,
+                                   int8_t mosipin, int8_t cspin,
+                                   int8_t resetpin) {
   // Software SPI
   _cs = cspin;
   _resetpin = resetpin;
@@ -63,24 +102,32 @@ Adafruit_CAP1188::Adafruit_CAP1188(int8_t clkpin, int8_t misopin,
   _i2c = false;
 }
 
+/*!
+ *    @brief  Setups the i2c depending on selected mode (I2C / SPI, Software / Hardware).
+ *            Displays useful debug info, as well as allow multiple touches (CAP1188_MTBLK), links leds to touches
+ *            (CAP1188_LEDLINK), and increase the cycle time value (CAP1188_STANDBYCFG) 
+ *    @param  i2caddr
+ *            i2caddres (default to 0x29)
+ *    @return True if initialization was successful, otherwise false.
+ */
 boolean Adafruit_CAP1188::begin(uint8_t i2caddr) {
   if (_i2c) {
     Wire.begin();
-    
+
     _i2caddr = i2caddr;
   } else if (_clk == -1) {
     // Hardware SPI
     pinMode(_cs, OUTPUT);
     digitalWrite(_cs, HIGH);
 #ifdef SPI_HAS_TRANSACTION
-      SPI.begin();
+    SPI.begin();
 #else
-      SPCRback = SPCR;
-      SPI.begin();
-      SPI.setClockDivider(SPI_CLOCK_DIV8);
-      SPI.setDataMode(SPI_MODE0);
-      mySPCR = SPCR;
-      SPCR = SPCRback;
+    SPCRback = SPCR;
+    SPI.begin();
+    SPI.setClockDivider(SPI_CLOCK_DIV8);
+    SPI.setDataMode(SPI_MODE0);
+    mySPCR = SPCR;
+    SPCR = SPCRback;
 #endif
   } else {
     // Sofware SPI
@@ -102,24 +149,23 @@ boolean Adafruit_CAP1188::begin(uint8_t i2caddr) {
   }
 
   readRegister(CAP1188_PRODID);
-  
+
   // Useful debugging info
-  
+
   Serial.print("Product ID: 0x");
   Serial.println(readRegister(CAP1188_PRODID), HEX);
   Serial.print("Manuf. ID: 0x");
   Serial.println(readRegister(CAP1188_MANUID), HEX);
   Serial.print("Revision: 0x");
   Serial.println(readRegister(CAP1188_REV), HEX);
-  
 
-  if ( (readRegister(CAP1188_PRODID) != 0x50) ||
-       (readRegister(CAP1188_MANUID) != 0x5D) ||
-       (readRegister(CAP1188_REV) != 0x83)) {
+  if ((readRegister(CAP1188_PRODID) != 0x50) ||
+      (readRegister(CAP1188_MANUID) != 0x5D) ||
+      (readRegister(CAP1188_REV) != 0x83)) {
     return false;
   }
   // allow multiple touches
-  writeRegister(CAP1188_MTBLK, 0); 
+  writeRegister(CAP1188_MTBLK, 0);
   // Have LEDs follow touches
   writeRegister(CAP1188_LEDLINK, 0xFF);
   // speed up a bit
@@ -127,7 +173,11 @@ boolean Adafruit_CAP1188::begin(uint8_t i2caddr) {
   return true;
 }
 
-uint8_t  Adafruit_CAP1188::touched(void) {
+/*!
+ *   @brief  Reads from CAP1188_SENINPUTSTATUS and if touch is detected writes to CAP1188_MAIN
+ *   @return Returns read from CAP1188_SENINPUTSTATUS
+ */
+uint8_t Adafruit_CAP1188::touched() {
   uint8_t t = readRegister(CAP1188_SENINPUTSTATUS);
   if (t) {
     writeRegister(CAP1188_MAIN, readRegister(CAP1188_MAIN) & ~CAP1188_MAIN_INT);
@@ -135,62 +185,66 @@ uint8_t  Adafruit_CAP1188::touched(void) {
   return t;
 }
 
+/*!
+ *   @brief  Controls the output polarity of LEDs.
+ *   @param  x
+ *           Writes x to CAP1188_LEDPOL register
+ */
 void Adafruit_CAP1188::LEDpolarity(uint8_t x) {
   writeRegister(CAP1188_LEDPOL, x);
 }
 
-/*********************************************************************/
-
-/**************************************************************************/
 /*!
-    @brief  Abstract away platform differences in Arduino wire library
-*/
-/**************************************************************************/
-static uint8_t i2cread(void) {
-  #if ARDUINO >= 100
+ *   @brief  Abstract away platform differences in Arduino wire library
+ */
+static uint8_t i2cread() {
+#if ARDUINO >= 100
   return Wire.read();
-  #else
+#else
   return Wire.receive();
-  #endif
+#endif
 }
 
-/**************************************************************************/
 /*!
     @brief  Abstract away platform differences in Arduino wire library
-*/
-/**************************************************************************/
+    @param  x
+ */
 static void i2cwrite(uint8_t x) {
-  #if ARDUINO >= 100
+#if ARDUINO >= 100
   Wire.write((uint8_t)x);
-  #else
+#else
   Wire.send(x);
-  #endif
+#endif
 }
 
-/**************************************************************************/
 /*!
-    @brief  Reads 8-bits from the specified register
-*/
-/**************************************************************************/
+ *   @brief  Reads 8-bits from the specified register
+ */
 uint8_t Adafruit_CAP1188::spixfer(uint8_t data) {
   if (_clk == -1) {
-   //Serial.println("Hardware SPI");
+    // Serial.println("Hardware SPI");
     return SPI.transfer(data);
   } else {
-   // Serial.println("Software SPI");
+    // Serial.println("Software SPI");
     uint8_t reply = 0;
-    for (int i=7; i>=0; i--) {
+    for (int i = 7; i >= 0; i--) {
       reply <<= 1;
       digitalWrite(_clk, LOW);
-      digitalWrite(_mosi, data & (1<<i));
+      digitalWrite(_mosi, data & (1 << i));
       digitalWrite(_clk, HIGH);
-      if (digitalRead(_miso)) 
-	reply |= 1;
+      if (digitalRead(_miso))
+        reply |= 1;
     }
     return reply;
   }
 }
 
+/*!
+ *    @brief  Reads from selected register
+ *    @param  reg
+ *            register address
+ *    @return
+ */
 uint8_t Adafruit_CAP1188::readRegister(uint8_t reg) {
   if (_i2c) {
     Wire.beginTransmission(_i2caddr);
@@ -200,7 +254,7 @@ uint8_t Adafruit_CAP1188::readRegister(uint8_t reg) {
     return (i2cread());
   } else {
 #ifdef SPI_HAS_TRANSACTION
-      spi_begin();
+    spi_begin();
 #else
     if (_clk == -1) {
       SPCRback = SPCR;
@@ -214,25 +268,26 @@ uint8_t Adafruit_CAP1188::readRegister(uint8_t reg) {
     digitalWrite(_cs, HIGH);
     digitalWrite(_cs, LOW);
     spixfer(0x7F);
-    uint8_t reply = spixfer(0); 
+    uint8_t reply = spixfer(0);
     digitalWrite(_cs, HIGH);
 #ifdef SPI_HAS_TRANSACTION
-      spi_end();
+    spi_end();
 #else
-      if (_clk == -1) {
-          SPCR = SPCRback;
-      }
+    if (_clk == -1) {
+      SPCR = SPCRback;
+    }
 #endif
     return reply;
-  }  
+  }
 }
 
-
-/**************************************************************************/
 /*!
-    @brief  Writes 8-bits to the specified destination register
-*/
-/**************************************************************************/
+ *   @brief  Writes 8-bits to the specified destination register
+ *   @param  reg
+ *           register address
+ *   @param  value
+ *           value that will be written at selected register
+ */
 void Adafruit_CAP1188::writeRegister(uint8_t reg, uint8_t value) {
   if (_i2c) {
     Wire.beginTransmission(_i2caddr);
@@ -241,12 +296,12 @@ void Adafruit_CAP1188::writeRegister(uint8_t reg, uint8_t value) {
     Wire.endTransmission();
   } else {
 #ifdef SPI_HAS_TRANSACTION
-      spi_begin();
+    spi_begin();
 #else
-      if (_clk == -1) {
-          SPCRback = SPCR;
-          SPCR = mySPCR;
-      }
+    if (_clk == -1) {
+      SPCRback = SPCR;
+      SPCR = mySPCR;
+    }
 #endif
     digitalWrite(_cs, LOW);
     // set address
@@ -258,11 +313,11 @@ void Adafruit_CAP1188::writeRegister(uint8_t reg, uint8_t value) {
     spixfer(value);
     digitalWrite(_cs, HIGH);
 #ifdef SPI_HAS_TRANSACTION
-      spi_end();
+    spi_end();
 #else
-      if (_clk == -1) {
-        SPCR = SPCRback;
-      }
+    if (_clk == -1) {
+      SPCR = SPCRback;
+    }
 #endif
   }
 }
--- Adafruit_CAP1188.h
+++ Adafruit_CAP1188.h
@@ -1,49 +1,76 @@
-/*************************************************** 
-  This is a library for the CAP1188 8-Channel Capacitive Sensor
-
-  Designed specifically to work with the CAP1188 breakout from Adafruit
-  ----> https://www.adafruit.com/products/1602
-
-  These sensors use I2C/SPI to communicate, 2+ pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+/*!
+ *  @file Adafruit_CAP1188.h
+ *
+ *  This is a library for the CAP1188 8-Channel Capacitive Sensor
+ *
+ *  Designed specifically to work with the CAP1188 I2C/SPI 8-chan Capacitive
+ * Sensor from Adafruit
+ *
+ *  Pick one up today in the adafruit shop!
+ *  ------> https://www.adafruit.com/product/1602
+ *
+ *  These sensors use I2C/SPI to communicate, 2+ pins are required to interface
+ *
+ *  Adafruit invests time and resources providing this open source code,
+ *  please support Adafruit andopen-source hardware by purchasing products
+ *  from Adafruit!
+ *
+ *  Limor Fried/Ladyada (Adafruit Industries).
+ *
+ *  BSD license, all text above must be included in any redistribution
+ */
 
 #if (ARDUINO >= 100)
- #include "Arduino.h"
+#include "Arduino.h"
 #else
- #include "WProgram.h"
+#include "WProgram.h"
 #endif
-#include <Wire.h>
 #include <SPI.h>
+#include <Wire.h>
 
-// The default I2C address
-#define CAP1188_I2CADDR 0x29
+#define CAP1188_I2CADDR 0x29 ///< The default I2C address
 
 // Some registers we use
-#define CAP1188_SENINPUTSTATUS 0x3
-#define CAP1188_MTBLK 0x2A
-#define CAP1188_LEDLINK 0x72
-#define CAP1188_PRODID 0xFD
-#define CAP1188_MANUID 0xFE
-#define CAP1188_STANDBYCFG 0x41
-#define CAP1188_REV 0xFF
-#define CAP1188_MAIN 0x00
-#define CAP1188_MAIN_INT 0x01
-#define CAP1188_LEDPOL 0x73
+#define CAP1188_SENINPUTSTATUS                                                 \
+  0x3 ///< The Sensor Input Status Register stores status bits that indicate a
+      ///< touch has been detected. A value of ‘0’ in any bit indicates that no
+      ///< touch has been detected. A value of ‘1’ in any bit indicates that a
+      ///< touch has been detected.
+#define CAP1188_MTBLK                                                          \
+  0x2A ///< Multiple Touch Configuration register controls the settings for the
+       ///< multiple touch detection circuitry. These settings determine the
+       ///< number of simultaneous buttons that may be pressed before additional
+       ///< buttons are blocked and the MULT status bit is set. [0/1]
+#define CAP1188_LEDLINK                                                        \
+  0x72 ///< Sensor Input LED Linking. Controls linking of sensor inputs to LED
+       ///< channels
+#define CAP1188_PRODID                                                         \
+  0xFD ///< Product ID. Stores a fixed value that identifies each product.
+#define CAP1188_MANUID                                                         \
+  0xFE ///< Manufacturer ID. Stores a fixed value that identifies SMSC
+#define CAP1188_STANDBYCFG                                                     \
+  0x41 ///< Standby Configuration. Controls averaging and cycle time while in
+       ///< standby. 
+#define CAP1188_REV                                                            \
+  0xFF ///< Revision register. Stores an 8-bit value that represents the part
+       ///< revision.
+#define CAP1188_MAIN                                                           \
+  0x00 ///< Main Control register. Controls the primary power state of the
+       ///< device.
+#define CAP1188_MAIN_INT                                                       \
+  0x01 ///< Main Control Int register. Indicates that there is an interrupt.
+#define CAP1188_LEDPOL                                                         \
+  0x73 ///< LED Polarity. Controls the output polarity of LEDs.
 
-
+/*!
+ *    @brief  Class that stores state and functions for interacting with
+ *            CAP1188 Sensor
+ */
 class Adafruit_CAP1188 {
- public:
+public:
   // Software SPI
-  Adafruit_CAP1188(int8_t clkpin, int8_t misopin, 
-		   int8_t mosipin,int8_t cspin, 
-		   int8_t resetpin);
+  Adafruit_CAP1188(int8_t clkpin, int8_t misopin, int8_t mosipin, int8_t cspin,
+                   int8_t resetpin);
   // Hardware SPI
   Adafruit_CAP1188(int8_t cspin, int8_t resetpin);
   // Hardware I2C
@@ -52,12 +79,12 @@ class Adafruit_CAP1188 {
   boolean begin(uint8_t i2caddr = CAP1188_I2CADDR);
   uint8_t readRegister(uint8_t reg);
   void writeRegister(uint8_t reg, uint8_t value);
-  uint8_t touched(void);
+  uint8_t touched();
   void LEDpolarity(uint8_t x);
 
- private:
+private:
   uint8_t spixfer(uint8_t x);
   boolean _i2c;
   int8_t _i2caddr, _resetpin, _cs, _clk, _mosi, _miso;
+  uint8_t mySPCR, SPCRback;
 };
-
